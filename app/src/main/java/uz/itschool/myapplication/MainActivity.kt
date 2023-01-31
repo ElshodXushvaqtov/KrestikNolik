55215b924456e16e1c3f41894f3280faee00131d
PC673 <elshodxushvaqtov07@gmail.com>
2023-01-31 15:16:50 +0500

    KrestikNolik Updated

--- app/src/main/java/uz/itschool/myapplication/MainActivity.kt
+++ app/src/main/java/uz/itschool/myapplication/MainActivity.kt
@@ -1,38 +1,154 @@
 package uz.itschool.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 var bool = true
-
+var matrix = Array(3) { IntArray(3) { -1 } }
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var image1 = findViewById<ImageView>(R.id.linear1image1).setOnClickListener(this)
-        var image2 = findViewById<ImageView>(R.id.linear1image2).setOnClickListener(this)
-        var image3 = findViewById<ImageView>(R.id.linear1image3).setOnClickListener(this)
-        var image4 = findViewById<ImageView>(R.id.linear2image1).setOnClickListener(this)
-        var image5 = findViewById<ImageView>(R.id.linear2image2).setOnClickListener(this)
-        var image6 = findViewById<ImageView>(R.id.linear2image3).setOnClickListener(this)
-        var image7 = findViewById<ImageView>(R.id.linear3image1).setOnClickListener(this)
-        var image8 = findViewById<ImageView>(R.id.linear3image2).setOnClickListener(this)
-        var image9 = findViewById<ImageView>(R.id.linear3image3).setOnClickListener(this)
+        linear1image1.setOnClickListener(this)
+        linear1image2.setOnClickListener(this)
+        linear1image3.setOnClickListener(this)
+        linear2image1.setOnClickListener(this)
+        linear2image2.setOnClickListener(this)
+        linear2image3.setOnClickListener(this)
+        linear3image1.setOnClickListener(this)
+        linear3image2.setOnClickListener(this)
+        linear3image3.setOnClickListener(this)
+        active_player.text = "Player X"
+        fun restart() {
+            linear1image1.isEnabled = true
+            linear1image2.isEnabled = true
+            linear1image3.isEnabled = true
+            linear2image1.isEnabled = true
+            linear2image2.isEnabled = true
+            linear2image3.isEnabled = true
+            linear3image1.isEnabled = true
+            linear3image2.isEnabled = true
+            linear3image3.isEnabled = true
+
+            linear1image1.setImageDrawable(null)
+            linear1image2.setImageDrawable(null)
+            linear1image3.setImageDrawable(null)
+            linear2image1.setImageDrawable(null)
+            linear2image2.setImageDrawable(null)
+            linear2image3.setImageDrawable(null)
+            linear3image1.setImageDrawable(null)
+            linear3image2.setImageDrawable(null)
+            linear3image3.setImageDrawable(null)
+            active_player.text = "Player X"
+        }
+        restart.setOnClickListener {
+            restart()
+        }
+
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onClick(view: View?) {
 
         val image = findViewById<ImageView>(view!!.id)
-        if (image.drawable != null) return
-        bool = if (bool) {
-            image.setImageResource(R.drawable.krestik)
-            false
-        } else {
-            image.setImageResource(R.drawable.nolik)
-            true
+        var tag = image.tag.toString().toInt()
+        var col: Int = tag / 3
+        var row: Int = tag % 3
+
+        fun finishGame() {
+            linear1image1.isEnabled = false
+            linear1image2.isEnabled = false
+            linear1image3.isEnabled = false
+            linear2image1.isEnabled = false
+            linear2image2.isEnabled = false
+            linear2image3.isEnabled = false
+            linear3image1.isEnabled = false
+            linear3image2.isEnabled = false
+            linear3image3.isEnabled = false
+            restart.visibility = View.VISIBLE
         }
+        fun isWinner(p: Int) {
+            var count = 0
+            for (i in 0..2) {
+                for (j in 0..2) {
+                    if (matrix[i][j] == p) {
+                        count++
+                    }
+                }
+                if (count == 3) {
+                    winner.text = "Winner is $p"
+                    finishGame()
+                    return
+                }
+                count = 0
+            }
+            count = 0
+            for (i in 0..2) {
+                for (j in 0..2) {
+                    if (i == j) {
+                        if (matrix[i][j] == p) {
+                            count++
+                        }
+                    }
+                }
+                if (count == 3) {
+                    winner.text = "Winner is $p"
+                    finishGame()
+                    return
+                }
+            }
+            count = 0
+            for (i in 0..2) {
+                for (j in 0..2) {
+                    if (i + j == 2) {
+                        if (matrix[i][j] == p) {
+                            count++
+                        }
+                    }
+                }
+                if (count == 3) {
+                    winner.text = "Winner is $p"
+                    finishGame()
+                    return
+                }
+            }
+            count = 0
+            for (i in 0..2) {
+                for (j in 0..2) {
+                    if (matrix[j][i] == p) {
+                        count++
+                    }
+                }
+                if (count == 3) {
+                    winner.text = "Winner is $p"
+                    finishGame()
+                    return
+                }
+                count = 0
+            }
+        }
+        if (matrix[col][row] == -1) {
+            if (bool) {
+                image.setImageResource(R.drawable.krestik)
+                bool = false
+                matrix[col][row] = 1
+                active_player.text = "Player-O"
+                isWinner(1)
+            } else {
+                image.setImageResource(R.drawable.nolik)
+                bool = true
+                matrix[col][row] = 0
+                active_player.text = "Player-X"
+                isWinner(0)
+            }
+        }
+
+
+
 
     }
 }
