f2611951f6bd6aac5016ef4adb20b4cb9decfc99
PC673 <elshodxushvaqtov07@gmail.com>
2023-02-01 09:59:15 +0500

    Animation

---
+++ app/src/main/java/uz/itschool/myapplication/intent_activity.kt
@@ -0,0 +1,15 @@
+package uz.itschool.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_intent.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class intent_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent)
+    }
+}
+
